49e2368d5c45a680d7970f7f9d0cf9c16159b2a2
Alan Souza <alan.souza@b2wdigital.com>
2019-05-28 10:27:59 -0300

    Finalização do curso - Parte 2

---
+++ src/app/rotas/livro-rotas.js
@@ -0,0 +1,31 @@
+
+const LivroControlador = require('../controladores/livro-controlador.js');
+const livroControlador = new LivroControlador();
+
+const Livro = require('../modelos/livro.js');
+
+const BaseControlador = require('../controladores/base-controlador.js');
+
+module.exports = (app) => {
+
+    const rotasLivro = LivroControlador.rotas();
+
+    app.use(rotasLivro.autenticadas, function(req, resp, next) {
+        if(req.isAuthenticated()) {
+            next();
+        } else {
+            resp.redirect(BaseControlador.rotas().login);
+        }
+    });
+
+    app.get(rotasLivro.lista, livroControlador.lista());
+
+    app.route(rotasLivro.cadastro)
+        .get(livroControlador.formularioCadastro())
+        .post(Livro.validacoes(), livroControlador.cadastra())
+        .put(livroControlador.edita());
+
+    app.get(rotasLivro.edicao, livroControlador.formularioEdicao());
+
+    app.delete(rotasLivro.delecao, livroControlador.remove());
+};
